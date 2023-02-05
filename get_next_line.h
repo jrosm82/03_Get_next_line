87588a87f5a295c51893f1d154281981d1d8c79d
Jan Rosmarin <jrosm82@gmail.com>
2023-02-05 17:57:01 +0100

    first commint nothing working

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrosmari <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/04 21:48:41 by jrosmari          #+#    #+#             */
+/*   Updated: 2023/02/05 13:55:26 by jrosmari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+
+char	*get_next_line(int fd, int BUFFER_SIZE);
+
+
+#endif
